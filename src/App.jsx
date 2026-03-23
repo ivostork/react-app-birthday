510f623769cdb7c0219201cd79da0e007c2af0ba
Ivo Stork <ivos.stork@gmail.com>
2026-03-23 13:34:31 +0100

    the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2 className="text-2xl">Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-lg bg-white p-4 space-y-8">
+        <h1 className="text-3xl">Card</h1>
+        <ul className="space-y-8">
+          {people.map((item) => {
+            const { id, name, age, image } = item;
+            return (
+              <li className="flex gap-4" key={id}>
+                <img
+                  src={image}
+                  alt=""
+                  className="w-16 h-16 object-cover rounded-full"
+                />
+                <div className="flex flex-col justify-center">
+                  <h3 className="text-2xl">{name}</h3>
+                  <p className="text-gray-400">{age}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          onClick={() => setPeople([])}
+          className="w-full inline-block py-1 bg-fuchsia-500 text-white"
+        >
+          Clear all
+        </button>
+      </div>
+    </div>
+  );
 };
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,32 +1,38 @@
+import img1 from "./assets/Bertie.png";
+import img2 from "./assets/Hester.png";
+import img3 from "./assets/Larry.png";
+import img4 from "./assets/Sean.png";
+import img5 from "./assets/Lola.png";
+
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: "Bertie Yates",
     age: 29,
-    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    image: img1,
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: "Hester Hogan",
     age: 32,
-    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    image: img2,
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: "Larry Little",
     age: 36,
-    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    image: img3,
   },
   {
     id: 4,
-    name: 'Sean Walsh',
+    name: "Sean Walsh",
     age: 34,
-    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    image: img4,
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: "Lola Gardner",
     age: 29,
-    image: 'https://www.course-api.com/images/people/person-5.jpeg',
+    image: img5,
   },
 ];
